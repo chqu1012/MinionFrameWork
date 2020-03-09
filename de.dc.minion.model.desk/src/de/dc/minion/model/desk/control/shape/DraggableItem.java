4023ae2d0b23903f2a866b0d26f2431b83dab408
chauq <chauq@LAPTOP-0387JN3P>
2020-03-09 19:36:06 +0100

    MFW-00007: Added DraggableItem

---
+++ de.dc.minion.model.desk/src/de/dc/minion/model/desk/control/shape/DraggableItem.java
@@ -0,0 +1,66 @@
+package de.dc.minion.model.desk.control.shape;
+
+import de.dc.minion.model.desk.util.DragResizeMod;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class DraggableItem extends Pane{ 
+	private BooleanProperty selectedProperty = new SimpleBooleanProperty();
+	
+	public DraggableItem(String name) {
+		Label label = new Label(name);
+		label.setPrefWidth(100);
+		label.setLayoutX(10);
+		label.setLayoutY(10);
+		getChildren().add(label);
+		DragResizeMod.makeResizable(this);
+		setPrefSize(100, 100);
+		
+		StringBinding css = Bindings.when(selectedProperty).
+				then("-fx-background-color: blue; -fx-border-color: yellow; -fx-border-width: 5").
+				otherwise("-fx-background-color: blue; -fx-border-color: transparent; -fx-border-width: 0");
+		styleProperty().bind(css);
+		
+		HBox hbox = new HBox(2);
+		hbox.getChildren().add(new Button("Edit"));
+		hbox.getChildren().add(new Button("Refresh"));
+		hbox.setLayoutY(-30);
+		
+		VBox vbox = new VBox(2);
+		Button buttonColor = new Button("Color");
+		Button buttonFont = new Button("Font");
+		Button buttonEffect = new Button("Effect");
+		buttonEffect.setMaxWidth(Double.MAX_VALUE);
+		buttonColor.setMaxWidth(Double.MAX_VALUE);
+		buttonFont.setMaxWidth(Double.MAX_VALUE);
+		vbox.getChildren().add(buttonColor);
+		vbox.getChildren().add(buttonFont);
+		vbox.getChildren().add(buttonEffect);
+		vbox.setLayoutX(-50);
+		selectedProperty.addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					getChildren().add(hbox);
+					getChildren().add(vbox);
+				}else {
+					getChildren().remove(hbox);
+					getChildren().remove(vbox);
+				}
+			}
+		});
+	}
+	
+	public BooleanProperty selectionProperty() {
+		return selectedProperty;
+	}
+}
