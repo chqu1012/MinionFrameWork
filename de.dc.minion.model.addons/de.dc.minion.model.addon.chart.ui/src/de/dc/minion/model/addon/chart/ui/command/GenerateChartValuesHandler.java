833b6f337a6a76262a97d2700126fecc5ccf0700
cqdsp <cqdsp2202@mail.com>
2020-03-16 21:05:18 +0100

    MFW-00038: Added generate chart value handler and menu item

---
+++ de.dc.minion.model.addons/de.dc.minion.model.addon.chart.ui/src/de/dc/minion/model/addon/chart/ui/command/GenerateChartValuesHandler.java
@@ -0,0 +1,42 @@
+package de.dc.minion.model.addon.chart.ui.command;
+
+import java.util.Random;
+
+import org.eclipse.fx.emf.edit.ui.AdapterFactoryTreeItem;
+
+import com.google.inject.Inject;
+
+import de.dc.javafx.xcore.workbench.chart.ChartFactory;
+import de.dc.javafx.xcore.workbench.chart.SeriesFX;
+import de.dc.javafx.xcore.workbench.chart.XYValueFX;
+import de.dc.minion.model.common.IControlManager;
+import de.dc.minion.model.common.command.ICommandHandler;
+import de.dc.minion.model.common.event.ISelectionService;
+import javafx.scene.control.TreeItem;
+
+public class GenerateChartValuesHandler implements ICommandHandler{
+
+	@Inject IControlManager controlManager;
+	@Inject ISelectionService selectionService;
+	
+	@Override
+	public void execute() {
+		Random random = new Random();
+		selectionService.getSelection().ifPresent(selection->{
+			if (selection instanceof TreeItem) {
+				TreeItem item = (TreeItem) selection;
+				Object value = item.getValue();
+				if (value instanceof SeriesFX) {
+					SeriesFX serieFX = (SeriesFX) value;
+					System.out.println(serieFX);
+					for (int i = 0; i < 20; i++) {
+						XYValueFX xy = ChartFactory.eINSTANCE.createXYValueFX();
+						xy.setX(i);
+						xy.setY(random.nextDouble());
+						serieFX.getValues().add(xy);					
+					}
+				}
+			}
+		});
+	}
+}
