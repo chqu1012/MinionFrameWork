da0ee67ca1f56c35777af4fe4fd22c81b1b14bf7
cqdsp <cqdsp2202@mail.com>
2020-04-24 14:01:42 +0200

    MFW-00111: FileView - Refactored file vision

--- de.dc.minion.model.addons/de.dc.minion.model.addon.fileview/src/de/dc/minion/model/addon/fileview/touch/FileVision.java
+++ de.dc.minion.model.addons/de.dc.minion.model.addon.fileview/src/de/dc/minion/model/addon/fileview/touch/FileVision.java
@@ -8,15 +8,12 @@ import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
 
 import de.dc.minion.model.addon.fileview.component.SimpleFileTreeItem;
-import de.dc.minion.model.addon.fileview.util.SystemIconsHelper;
+import de.dc.minion.model.addon.fileview.component.cell.FileTreeCell;
 import de.dc.minion.model.common.control.EmfViewPart;
 import de.dc.minion.model.common.event.EventContext;
 import de.dc.minion.model.common.event.IEventBroker;
 import javafx.scene.Parent;
-import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeView;
-import javafx.scene.image.ImageView;
-import javafx.util.Callback;
 
 public class FileVision extends EmfViewPart {
 
@@ -26,28 +23,7 @@ public class FileVision extends EmfViewPart {
 	@Override
 	public Parent create() {
 		TreeView<File> fileView = new TreeView<>(new SimpleFileTreeItem(new File("C:\\")));
-		fileView.setCellFactory(new Callback<TreeView<File>, TreeCell<File>>() {
-			@Override
-			public TreeCell<File> call(TreeView<File> param) {
-				return new TreeCell<File>() {
-					@Override
-					protected void updateItem(File item, boolean empty) {
-						super.updateItem(item, empty);
-						if (item == null || empty) {
-							setText(null);
-							setGraphic(null);
-						}else {
-							setText(item.getName());
-							if (item.isDirectory()) {
-								setGraphic(new ImageView("/de/dc/minion/model/addon/fileview/images/icons8-mappe-16.png"));
-							}else {
-								setGraphic(new ImageView(SystemIconsHelper.getFileIcon(item.getName())));
-							}
-						}
-					}
-				};
-			}
-		});
+		fileView.setCellFactory(cell -> new FileTreeCell());
 		return fileView;
 	}
 
