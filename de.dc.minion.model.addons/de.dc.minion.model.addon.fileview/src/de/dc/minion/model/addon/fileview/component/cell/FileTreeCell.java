0a0c15e4a4c64dc11c2ceb8b4eafe834601b2df9
cqdsp <cqdsp2202@mail.com>
2020-04-25 02:46:52 +0200

    MFW-00114: FileView - Children count in folder

--- de.dc.minion.model.addons/de.dc.minion.model.addon.fileview/src/de/dc/minion/model/addon/fileview/component/cell/FileTreeCell.java
+++ de.dc.minion.model.addons/de.dc.minion.model.addon.fileview/src/de/dc/minion/model/addon/fileview/component/cell/FileTreeCell.java
@@ -1,6 +1,7 @@
 package de.dc.minion.model.addon.fileview.component.cell;
 
 import java.io.File;
+import java.util.Arrays;
 
 import de.dc.minion.model.addon.fileview.util.SystemIconsHelper;
 import javafx.scene.control.TreeCell;
@@ -17,10 +18,12 @@ public class FileTreeCell extends TreeCell<File> {
 			setText(null);
 			setGraphic(null);
 		} else {
-			setText(item.getName());
 			if (item.isDirectory()) {
+				long count = Arrays.asList(item.listFiles()).stream().filter(File::isFile).count();
+				setText(String.format("%s (%s Files)", item.getName(), count));
 				setGraphic(new ImageView(IMG_FOLDER_URL));
 			} else {
+				setText(item.getName());
 				setGraphic(new ImageView(SystemIconsHelper.getFileIcon(item.getName())));
 			}
 		}
