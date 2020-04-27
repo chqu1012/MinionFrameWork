43c211cc1ea3e32c08474faa8af98ffe278b87c2
cqdsp <cqdsp2202@mail.com>
2020-04-27 10:33:19 +0200

    MFW-00123: Metro - Refactoring ribbon control

--- de.dc.minion.model.desk.metro/src/de/dc/minion/model/desk/metro/component/MinionRibbonControl.java
+++ de.dc.minion.model.desk.metro/src/de/dc/minion/model/desk/metro/component/MinionRibbonControl.java
@@ -4,16 +4,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.io.Charsets;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.eclipse.fx.ui.controls.styledtext.StyledTextArea;
+
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
 
@@ -44,24 +46,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 
-public class MinionRibbonControl extends BaseMinionRibbonControl{
+public class MinionRibbonControl extends BaseMinionRibbonControl {
 
 	public static final String ID = "de.dc.minion.model.desk.metro.component.MinionRibbonControl";
-	
+
 	private static final Logger LOG = Logger.getLogger(MinionRibbonControl.class.getName());
-	
+
 	protected ILandscapeFX currentLandscape;
 	protected Map<Landscape, ILandscapeFX> perspectiveManager = new HashMap<>();
-	
+
 	protected ObservableList<Landscape> landscapes = FXCollections.observableArrayList();
 	protected FilteredList<Landscape> filteredLandscapes = new FilteredList<Landscape>(landscapes);
-	
+
 	@Inject
 	protected MinionFile minionFile;
 
@@ -70,13 +73,16 @@ public class MinionRibbonControl extends BaseMinionRibbonControl{
 
 	@Inject
 	protected ISelectionService selectionService;
-	
+
 	@Inject
 	protected MinionRibbonBuilder minionBuilder;
 
 	@Inject
 	protected IControlManager controlManager;
 	
+	@Inject
+	protected IEmfFileManager fileManager;
+
 	public MinionRibbonControl() {
 		MinionPlatform.inject(this);
 		eventBroker.register(this);
@@ -84,25 +90,23 @@ public class MinionRibbonControl extends BaseMinionRibbonControl{
 		loadFxml();
 		registerTourches();
 	}
-	
+
 	public void initialize() {
 		listViewLandscapes.setItems(filteredLandscapes);
 		listViewLandscapes.setCellFactory(param -> new LandscapeListCell());
-		
-		textSearchlandscapes.textProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if (newValue!=null) {
-					filteredLandscapes.setPredicate(p->{
-						boolean isEmpty = p.getName().isEmpty() || p.getName()==null;
-						boolean containsName = p.getName().toLowerCase().contains(newValue.toLowerCase());
-						return isEmpty || containsName;
-					});
-				}
-			}
-		});
+		textSearchlandscapes.textProperty().addListener(this::onTextSearchLandscapes);
 	}
 
+	private void onTextSearchLandscapes(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		if (newValue != null) {
+			filteredLandscapes.setPredicate(p -> {
+				boolean isEmpty = p.getName().isEmpty() || p.getName() == null;
+				boolean containsName = p.getName().toLowerCase().contains(newValue.toLowerCase());
+				return isEmpty || containsName;
+			});
+		}
+	}
+	
 	private void loadFxml() {
 		String fxmlName = "/de/dc/minion/model/desk/metro/MinionRibbon.fxml";
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlName));
@@ -113,36 +117,35 @@ public class MinionRibbonControl extends BaseMinionRibbonControl{
 			fxmlLoader.load();
 		} catch (IOException exception) {
 			LOG.log(Level.SEVERE, "Error loading fxml " + exception.getLocalizedMessage());
-		}		
+		}
 	}
 
 	public void registerTourches() {
 		ClassLoader cl = ClassLoader.getSystemClassLoader();
 		URL[] urls = ((URLClassLoader) cl).getURLs();
+		Arrays.asList(urls).stream().forEach(e -> loadMinionFile(e.getFile()));
+	}
 
-		for (URL url : urls) {
-			String file = url.getFile();
-			file = file + "desk.minion";
-			File minionFile = new File(file);
-			if (minionFile.exists()) {
-				MinionFile efile = new MinionFile();
-				Minion minion = efile.load(minionFile.getAbsolutePath());
-				Toady toady = minion.getToadies().get(0);
-				if (toady.getStatus()==ToadyStatus.STARTED) {
-					minionBuilder.setDesk(this);
-					minionBuilder.doSwitch(minion);
-				}
+	private void loadMinionFile(String file) {
+		file = file + "desk.minion";
+		File minionFile = new File(file);
+		if (minionFile.exists()) {
+			MinionFile efile = new MinionFile();
+			Minion minion = efile.load(minionFile.getAbsolutePath());
+			Toady toady = minion.getToadies().get(0);
+			if (toady.getStatus() == ToadyStatus.STARTED) {
+				minionBuilder.setDesk(this);
+				minionBuilder.doSwitch(minion);
 			}
 		}
 	}
-	
+
 	@Override
 	protected void onButtonAction(ActionEvent event) {
 		Object source = event.getSource();
 		if (source == buttonOpen) {
 			stackPaneMain.getChildren().add(new Button("Open"));
-		}
-		else if (source == buttonNew) {
+		} else if (source == buttonNew) {
 			stackPaneMain.getChildren().add(new Button("New"));
 		}
 	}
@@ -157,14 +160,14 @@ public class MinionRibbonControl extends BaseMinionRibbonControl{
 	@Override
 	protected void onMouseClicked(MouseEvent event) {
 		Object source = event.getSource();
-		if (event.getClickCount()==2) {
+		if (event.getClickCount() == 2) {
 			if (source == listViewLandscapes) {
 				Landscape selection = listViewLandscapes.getSelectionModel().getSelectedItem();
 				switchPerspective(selection);
 			}
 		}
 	}
-	
+
 	@Subscribe
 	public void openFile(EventContext<?> context) {
 		Object input = context.getInput();
@@ -174,51 +177,63 @@ public class MinionRibbonControl extends BaseMinionRibbonControl{
 			if (!filename.isEmpty()) {
 				if (!isFileOpen(filename)) {
 					String extension = FilenameUtils.getExtension(filename);
-					Optional<IEmfEditorPart<?>> editorPart = MinionPlatform.getInstance(IEmfFileManager.class)
-							.getEditorByExtension(extension);
-					editorPart.ifPresent(e -> {
-						try {
-							IEmfEditorPart<?> editor = e.getClass().newInstance();
-							MinionPlatform.inject(editor);
-							Tab editorTab = new Tab(file.getName());
-							editor.load(file);
-							editorTab.setContent((Node) editor);
-							currentLandscape.getEditorArea().getTabs().add(editorTab);
-							currentLandscape.getEditorArea().getSelectionModel().select(editorTab);
-						} catch (InstantiationException | IllegalAccessException e1) {
-							LOG.log(Level.SEVERE, "Failed to open editor "+filename);
-						}
-					});
+					Optional<IEmfEditorPart<?>> editorPart = fileManager.getEditorByExtension(extension);
+					editorPart.ifPresent(e -> createEditor(file, filename, e));
 					if (!editorPart.isPresent()) {
-						Alert alert = new Alert(AlertType.CONFIRMATION);
-						alert.setTitle("File Extension "+extension+" not registered");
-						alert.setHeaderText("Toady not loaded or not available");
-						alert.setContentText("Do you want to open the Touch Manager?");
-
-						Optional<ButtonType> result = alert.showAndWait();
-						if (result.get() == ButtonType.OK){
-							MinionPlatform.getInstance(IEventBroker.class).post(new EventContext<>(MinionConstants.EVENT_OPEN_LANDSCAPE_ID, MinionConstants.LANDSCAPE_TOUCH_MANAGER));
-						}
+						openDialogForTouchManager(extension);
 					}
 				} else {
-					getTabByName(filename)
-							.ifPresent(e -> currentLandscape.getEditorArea().getSelectionModel().select(e));
+					getTabByName(filename).ifPresent(e -> {
+						SingleSelectionModel<Tab> selectionModel = currentLandscape.getEditorArea().getSelectionModel();
+						selectionModel.select(e);
+					});
 				}
 			} else {
-				StyledTextArea styledTextArea = new StyledTextArea();
-				try {
-					styledTextArea.getContent().setText(FileUtils.readFileToString(file, Charsets.UTF_8));
-					Tab editorTab = new Tab(filename);
-					editorTab.setContent(styledTextArea);
-					currentLandscape.getEditorArea().getTabs().add(editorTab);
-					currentLandscape.getEditorArea().getSelectionModel().select(editorTab);
-				} catch (IOException e1) {
-					LOG.log(Level.SEVERE, "Failed to open file "+filename);
-				}
+				createEditorDefault(file, filename);
 			}
 		}
 	}
-	
+
+	private void openDialogForTouchManager(String extension) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("File Extension " + extension + " not registered");
+		alert.setHeaderText("Toady not loaded or not available");
+		alert.setContentText("Do you want to open the Touch Manager?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			eventBroker.post(new EventContext<>(MinionConstants.EVENT_OPEN_LANDSCAPE_ID,
+					MinionConstants.LANDSCAPE_TOUCH_MANAGER));
+		}
+	}
+
+	private void createEditorDefault(File file, String filename) {
+		StyledTextArea styledTextArea = new StyledTextArea();
+		try {
+			styledTextArea.getContent().setText(FileUtils.readFileToString(file, StandardCharsets.UTF_8));
+			Tab editorTab = new Tab(filename);
+			editorTab.setContent(styledTextArea);
+			currentLandscape.getEditorArea().getTabs().add(editorTab);
+			currentLandscape.getEditorArea().getSelectionModel().select(editorTab);
+		} catch (IOException e1) {
+			LOG.log(Level.SEVERE, "Failed to open file " + filename);
+		}
+	}
+
+	private void createEditor(File file, String filename, IEmfEditorPart<?> e) {
+		try {
+			IEmfEditorPart<?> editor = e.getClass().newInstance();
+			MinionPlatform.inject(editor);
+			Tab editorTab = new Tab(file.getName());
+			editor.load(file);
+			editorTab.setContent((Node) editor);
+			currentLandscape.getEditorArea().getTabs().add(editorTab);
+			currentLandscape.getEditorArea().getSelectionModel().select(editorTab);
+		} catch (InstantiationException | IllegalAccessException e1) {
+			LOG.log(Level.SEVERE, "Failed to open editor " + filename);
+		}
+	}
+
 	public boolean isFileOpen(String name) {
 		return currentLandscape.getEditorArea().getTabs().stream().anyMatch(e -> e.getText().equalsIgnoreCase(name));
 	}
@@ -227,7 +242,7 @@ public class MinionRibbonControl extends BaseMinionRibbonControl{
 		return currentLandscape.getEditorArea().getTabs().stream().filter(e -> e.getText().equalsIgnoreCase(name))
 				.findAny();
 	}
-	
+
 	public void switchPerspective(Landscape landscape) {
 		ILandscapeFX openLandscape = perspectiveManager.get(landscape);
 		if (openLandscape != null) {
